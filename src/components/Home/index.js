950377eaf7246886eceb2dc3a20a730083be450e
sureshnenavath <suryanss143@gmail.com>
2025-03-08 14:04:33 +0000

    Initial Commit

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -3,7 +3,7 @@ import BlogList from '../BlogList'
 import UserInfo from '../UserInfo'
 
 const Home = () => (
-  <div className="home-container" data-testid="loader">
+  <div className="home-container">
     <UserInfo />
     <BlogList />
   </div>
